23db3412fcd6a8368edd4bc18072a34d8d07016f
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-08-20 17:53:40 +0530

    Add files via upload

---
+++ 26.java
@@ -0,0 +1,17 @@
+package javaset2;
+ class jim{
+	float salary=40000;  
+		}  
+		class javalab026 extends jim  {  
+		 int bonus=10000;  
+		 public static void main(String args[]){  
+			 javalab026 p=new javalab026();  
+	   System.out.println("Programmer salary is:"+p.salary);  
+		   System.out.println("Bonus of Programmer is:"+p.bonus);  
+		}  
+
+	
+
+}
+
+
