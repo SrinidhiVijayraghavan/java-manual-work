a09872a2d51bcff4b5b4f7fa86cafc419ca76df4
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-09-02 00:13:18 +0530

    Add files via upload

---
+++ zoneoffsetclass.java
@@ -0,0 +1,9 @@
+  import java.time.*;  
+import java.time.temporal.Temporal;  
+public class ZoneOffsetExample1 {  
+  public static void main(String[] args) {  
+    ZoneOffset zone = ZoneOffset.UTC;  
+    Temporal temp = zone.adjustInto(ZonedDateTime.now());  
+    System.out.println(temp);  
+  }  
+}  
