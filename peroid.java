e3af60d1432051de4a9f0a1fa444a71f96c9b7b8
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-09-02 00:30:34 +0530

    Add files via upload

---
+++ peroid.java
@@ -0,0 +1,9 @@
+import java.time.*;  
+import java.time.temporal.Temporal;  
+public class PeriodExample1 {  
+  public static void main(String[] args) {  
+    Period period = Period.ofDays(24);  
+    Temporal temp = period.addTo(LocalDate.now());  
+    System.out.println(temp);  
+  }  
+}  
