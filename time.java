18b13911f8d1559c0c03e79c5408ffb589ced1c4
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-09-01 23:21:37 +0530

    Add files via upload

---
+++ time.java
@@ -0,0 +1,8 @@
+import java.time.*;  
+public class LocalDateExample3 {  
+  public static void main(String[] args) {  
+    LocalDate date = LocalDate.of(2017, 1, 13);  
+    LocalDateTime datetime = date.atTime(1,50,9);      
+    System.out.println(datetime);   
+  }  
+}  
