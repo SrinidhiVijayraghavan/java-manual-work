766ccf76b3e272356242ad84e6ebdc8fd1b8d511
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-09-02 00:13:51 +0530

    Add files via upload

---
+++ zoneoffsetclasshours.java
@@ -0,0 +1,7 @@
+import java.time.ZoneOffset;  
+public class ZoneOffsetExample2 {  
+  public static void main(String[] args) {  
+    ZoneOffset zone = ZoneOffset.ofHours(5);  
+    System.out.println(zone);  
+  }  
+}  
