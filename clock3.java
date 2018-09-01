1600f75d5d20e13d1831a4433296b4cb3e7fc60a
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-09-02 00:03:50 +0530

    Add files via upload

---
+++ clock3.java
@@ -0,0 +1,7 @@
+import java.time.Clock;  
+public class ClockExample2 {  
+  public static void main(String[] args) {  
+    Clock c = Clock.systemUTC();  
+    System.out.println(c.instant());  
+  }  
+}  
