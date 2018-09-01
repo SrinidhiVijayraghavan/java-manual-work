830a97337186cd7597beef53af63cd5a27ea2659
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-09-01 23:22:45 +0530

    Add files via upload

---
+++ time6.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;  
+import java.time.format.DateTimeFormatter;  
+public class LocalDateTimeExample2 {  
+  public static void main(String[] args) {  
+    LocalDateTime datetime1 = LocalDateTime.now();  
+    DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");  
+    String formatDateTime = datetime1.format(format);   
+    System.out.println(formatDateTime);    
+  }  
+}  
