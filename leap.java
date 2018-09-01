87cf38f7cafa006e020a77aaa3fd9a156f62d10c
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-09-02 00:04:37 +0530

    Add files via upload

---
+++ leap.java
@@ -0,0 +1,7 @@
+import java.time.Year;  
+public class YearExample4 {  
+  public static void main(String[] args) {  
+    Year year = Year.of(2016);  
+    System.out.println(year.isLeap());  
+  }  
+}  
