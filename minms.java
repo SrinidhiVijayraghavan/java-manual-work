50a8b879e33fc50631dbb0459103d8c4acc5b426
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-09-01 23:17:40 +0530

    Add files via upload

---
+++ minms.java
@@ -0,0 +1,8 @@
+import java.time.OffsetDateTime;  
+public class OffsetDateTimeExample5 {  
+  public static void main(String[] args) {  
+    OffsetDateTime offset = OffsetDateTime.now();  
+    OffsetDateTime value =  offset.minusDays(240);  
+    System.out.println(value);  
+  }   
+}  
