c4137243f5ae347aac15284f935a8a2b10c3f2e7
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-09-02 00:04:54 +0530

    Add files via upload

---
+++ zc1.java
@@ -0,0 +1,7 @@
+import java.time.ZoneId;  
+public class ZoneIdExample2 {  
+  public static void main(String[] args) {  
+    ZoneId zone = ZoneId.systemDefault();     
+    System.out.println(zone);  
+  }       
+}  
