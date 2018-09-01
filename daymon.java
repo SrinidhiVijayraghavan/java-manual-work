279c0fc719dece8e0eb49f34971df6dd16b99b01
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-09-01 23:13:52 +0530

    Add files via upload

---
+++ daymon.java
@@ -0,0 +1,7 @@
+import java.time.OffsetDateTime;  
+public class OffsetDateTimeExample1 {  
+    public static void main(String[] args) {  
+        OffsetDateTime offsetDT = OffsetDateTime.now();  
+        System.out.println(offsetDT.getDayOfMonth());  
+    }  
+}  
