a0ec6c16e123550c97dc3b53eaafe72ba263b863
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-09-01 23:20:29 +0530

    Add files via upload

---
+++ offset3.java
@@ -0,0 +1,8 @@
+import java.time.OffsetTime;  
+public class OffsetTimeExample4 {  
+  public static void main(String[] args) {  
+    OffsetTime offset = OffsetTime.now();  
+    int s = offset.getSecond();  
+    System.out.println(s+ " second");  
+  }  
+}  
