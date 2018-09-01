8188bcac61eca22267e28d2372cff285a1dcfe24
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-09-01 23:24:47 +0530

    Add files via upload

---
+++ tolocaldate.java
@@ -0,0 +1,7 @@
+import java.time.OffsetDateTime;  
+public class OffsetDateTimeExample4 {  
+    public static void main(String[] args) {  
+        OffsetDateTime offsetDT = OffsetDateTime.now();  
+        System.out.println(offsetDT.toLocalDate());  
+    }  
+}  
