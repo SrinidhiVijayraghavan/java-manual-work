5189465f7bbb6d72eb1813b9829c388faee63726
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-09-02 00:11:09 +0530

    Add files via upload

---
+++ zone1.java
@@ -0,0 +1,7 @@
+import java.time.ZonedDateTime;  
+public class ZonedDateTimeExample1{  
+  public static void main(String[] args) {  
+    ZonedDateTime zone = ZonedDateTime.parse("2016-10-05T08:20:10+05:30[Asia/Kolkata]");  
+    System.out.println(zone);  
+  }  
+}  
