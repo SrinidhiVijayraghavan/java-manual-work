1cde6738c8677aafe23871d071eeec0ff946a0ad
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-09-01 23:18:34 +0530

    Add files via upload

---
+++ mondayclass.java
@@ -0,0 +1,11 @@
+import java.time.*;  
+import java.time.temporal.*;  
+public class MonthDayExample4 {  
+  public static void main(String[] args) {  
+    MonthDay month = MonthDay.now();  
+    ValueRange r1 = month.range(ChronoField.MONTH_OF_YEAR);  
+    System.out.println(r1);  
+    ValueRange r2 = month.range(ChronoField.DAY_OF_MONTH);  
+    System.out.println(r2);  
+  }  
+}  
