6755786e7ebdbfead705e4514f13e8bf437302c3
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-09-02 13:20:15 +0530

    Add files via upload

---
+++ 76.java
@@ -0,0 +1,6 @@
+public class StartsWithExample{  
+public static void main(String args[]){  
+String s="srinidhi";  
+System.out.println(s.startsWith("ka"));  
+System.out.println(s.endsWith("sh"));  
+}}  
